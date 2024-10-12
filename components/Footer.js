5751c5f166e63bd552b29ef008dab1c69a1f2d1e
Khushi Patel <98079482+Khushi2184@users.noreply.github.com>
2024-10-12 16:21:38 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+// components/Footer.js
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white p-4 text-center">
+            <p>&copy; 2024 Hospital Management System. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
