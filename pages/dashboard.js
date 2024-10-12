87e070718de55a313dda404af9b6bdba357b88d9
Khushi Patel <98079482+Khushi2184@users.noreply.github.com>
2024-10-12 16:28:01 +0530

    Create dashboard.js

---
+++ pages/dashboard.js
@@ -0,0 +1,16 @@
+// pages/dashboard.js
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Dashboard from '../components/Dashboard';
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <Header />
+            <Dashboard />
+            <Footer />
+        </div>
+    );
+};
+
+export default DashboardPage;
